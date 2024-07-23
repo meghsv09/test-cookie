fac6ad7d6e68ba7de016455e6d1dfcffcfe798e4
meghsv09 <168646296+meghsv09@users.noreply.github.com>
2024-07-23 13:41:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ import { cognitoUserPoolsTokenProvider } from 'aws-amplify/auth/cognito';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
-//Amplify.configure(config);
+Amplify.configure(config);
 
-Amplify.configure({...config,
-  Auth: {
-          cookieStorage: {
-    // - Cookie domain (only required if cookieStorage is provided)
-    domain: '*.meghasv.in',
-    // (optional) - Cookie path
-    path: '/',
-    // (optional) - Cookie expiration in days
-    expires: 365,
-    // (optional) - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
-    sameSite: 'lax',
-    secure: true
-    // (optional) - Cookie secure flag
-    // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-  },
-}
-})
+// Amplify.configure({...config,
+//   Auth: {
+//           cookieStorage: {
+//     // - Cookie domain (only required if cookieStorage is provided)
+//     domain: '*.meghasv.in',
+//     // (optional) - Cookie path
+//     path: '/',
+//     // (optional) - Cookie expiration in days
+//     expires: 365,
+//     // (optional) - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+//     sameSite: 'lax',
+//     secure: true
+//     // (optional) - Cookie secure flag
+//     // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+//   },
+// }
+// })
 
-cognitoUserPoolsTokenProvider.setKeyValueStorage(new CookieStorage());
+cognitoUserPoolsTokenProvider.setKeyValueStorage(new CookieStorage({ domain: '.meghasv.in'}));
 
 function App({ signOut, user }) {
   return (
